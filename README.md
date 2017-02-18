b751ffd1e0a695463b1f2b525740829912dfd83a
Miguel Laginha <me@miguellaginha.com>
2017-02-18 12:45:10 +0000

    Bugfix: italics and bolds were not being included in node parsing

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 ![Ah e tal agora escreve-se assim...](https://raw.github.com/brecke/desacordo_ortografico/master/images/fuck-that-bitch-yao-ming.png)
 
-# Desacordo Ortográfico #
+# Desacordo Ortográfico
 
 Este repositório tem 3 pastas:
 
@@ -8,7 +8,7 @@ Este repositório tem 3 pastas:
 * `bookmarklet` tem o mesmo código em formato de *bookmarklet*, para ser usado noutros *browsers*
 * `scripts` inclui o código python usado para criar o ficheiro `mappings.js`, que tem os mapeamentos do acordo de 1990 para o de 1945.
 
-## Porquê ##
+## Porquê
 
 Não há coisa mais aberrante do que uns mentecaptos a tentar "evoluir" a
 língua à força e por decreto. Esta idiotice tem de acabar (e rápido), e esta
@@ -17,11 +17,11 @@ ortográfico.
 
 Voltemos a ler em bom Português.
 
-## Posso contribuir? ##
+## Posso contribuir?
 
 Claro. Há duas formas de o fazer:
 
-1. Identificar erros e/ou situações em que a extensão não funciona correctamente. Para isso basta abrir um *issue* e descrever o cenário.
+1. Identificar erros e/ou situações em que a extensão não funciona correctamente. Para isso basta abrir um *issue* ou enviar um e-mail e descrever a situação.
 2. Fazer *fork* e *pull request* com melhorias ao código. Cada pasta tem um ficheiro `TODO.md` com ideias de ajustes e correcções a fazer.
 
 
--- chrome extension/translate.js
+++ chrome extension/translate.js
@@ -1,57 +1,50 @@
 function isCapitalized(string) {
-    first = string[0];
-    rest  = string.slice(1);
-    return first==first.toUpperCase() && rest==rest.toLowerCase();
+  first = string[0];
+  rest = string.slice(1);
+  return first == first.toUpperCase() && rest == rest.toLowerCase();
 }
 
 function capitalize(string) {
-    return string[0].toUpperCase() + string.slice(1).toLowerCase();
+  return string[0].toUpperCase() + string
+    .slice(1)
+    .toLowerCase();
 }
 
 function isLowerCase(string) {
-    return string == string.toLowerCase();
+  return string == string.toLowerCase();
 }
 
 function isUpperCase(string) {
-    return string == string.toUpperCase();
+  return string == string.toUpperCase();
 }
 
 // go through the DOM recursively
 function getTextNodesIn(node, includeWhitespaceNodes) {
-    var textNodes = [], whitespace = /^\s*$/;
+  var textNodes = [];
+  var whitespace = /^\s*$/;
 
-    function getTextNodes(node) {
-
-        if (node.nodeType == 3) {
-            if (includeWhitespaceNodes || (!whitespace.test(node.textContent))) { // nodeValue
-              textNodes.push(node);
-            }
-
-        // recursively get the text nodes from other node types:
-        // TYPE 1: ELEMENT_NODE
-        // exception for <script>
-        // exception for <style>
-        // exception for <em>
-        // exception for <i>
-        // exception for <b>
-        // exception for <strong>
-        } else if (node.nodeType == 1
-          && node.nodeName.toLowerCase() != "script"
-          && node.nodeName.toLowerCase() != 'style'
-          // && node.nodeName.toLowerCase() != 'strong'
-          // && node.nodeName.toLowerCase() != 'em'
-          && node.nodeName.toLowerCase() != 'i'
-          // && node.nodeName.toLowerCase() != 'b'
-          ) {
-
-          for (var i = 0, len = node.childNodes.length; i < len; ++i) {
-              getTextNodes(node.childNodes[i]);
-          }
-        }
+  function collectNodes(node) {
+    if (includeWhitespaceNodes || (!whitespace.test(node.textContent))) { // nodeValue
+        textNodes.push(node);
     }
+  }
 
-    getTextNodes(node);
-    return textNodes;
+  function getTextNodes(node) {
+    // it's just words
+    if (node.nodeType == 3) {
+      collectNodes(node);
+    } else if (node.nodeType === 1 && (node.nodeName.toLowerCase() === 'i' || node.nodeName.toLowerCase() === 'b' || node.nodeName.toLowerCase() === 'strong' || node.nodeName.toLowerCase() === 'em')) { // in case it's an element, but it's just style
+      var newNode = document.createTextNode(node.textContent.replace(/(<([^>]+)>)/ig, ''));
+      collectNodes(newNode);    
+    } else if (node.nodeType === 1 && node.nodeName.toLowerCase() != "script" && node.nodeName.toLowerCase() != 'style') { // in case it's an element with other elements in it (possibly)
+      for (var i = 0, len = node.childNodes.length; i < len; ++i) {
+        getTextNodes(node.childNodes[i]);
+      }
+    }
+  }
+
+  getTextNodes(node);
+  return textNodes;
 }
 
 function translate(createOverlay, removeOverlay) {
@@ -59,59 +52,94 @@ function translate(createOverlay, removeOverlay) {
   createOverlay();
 
   var textNodes = getTextNodesIn(document.body, false);
-  var text, re;
-  
-  for(var i = 0; i < textNodes.length; i++) {
+  var text,
+    re;
+
+  for (var i = 0; i < textNodes.length; i++) {
 
     text = $.trim(textNodes[i].textContent);
-    
+
     flag = false;
-    if (text.indexOf('Cole')>=0) {
-        flag = true;
+    if (text.indexOf('Cole') >= 0) {
+      flag = true;
     };
 
-    toReplace = [':', '.', '?', '_', '@', '?', '"', "'", '”', '“', '‘', '’', ';', '(', ')', '«', '»', ',', '!'];
-    for (var j = 0; j < toReplace.length; j++) {
-        // replace via regex
-        re = new RegExp('\\'+toReplace[j], 'g');
-        text = text.replace(re, ' ');
-    };
-    
+    toReplace = [
+      ':',
+      '.',
+      '?',
+      '_',
+      '@',
+      '?',
+      '"',
+      "'",
+      '”',
+      '“',
+      '‘',
+      '’',
+      ';',
+      '(',
+      ')',
+      '«',
+      '»',
+      ',',
+      '!'
+    ];
+    for (var x = 0; x < toReplace.length; x++) {
+      // replace via regex
+      re = new RegExp('\\' + toReplace[x], 'g');
+      text = text.replace(re, ' ');
+    }
+
     text = $.trim(text);
     tokens = text.split(/[\s,]+/);
 
     for (var j = 0; j < tokens.length; j++) {
       word = tokens[j];
-      replaceWord = mappings[ word.toLowerCase() ];
+      replaceWord = mappings[word.toLowerCase()];
 
-       if (replaceWord) {
-           if (isCapitalized(word)) {
-               textNodes[i].textContent = textNodes[i].textContent.replace(word, capitalize( replaceWord ));
-           } else if (isUpperCase(word)) {
-               textNodes[i].textContent = textNodes[i].textContent.replace(word, replaceWord.toUpperCase());
-           } else {
-               textNodes[i].textContent = textNodes[i].textContent.replace(word, replaceWord);
-           };
-       };
+      if (replaceWord) {
+        if (isCapitalized(word)) {
+          textNodes[i].textContent = textNodes[i]
+            .textContent
+            .replace(word, capitalize(replaceWord));
+        } else if (isUpperCase(word)) {
+          textNodes[i].textContent = textNodes[i]
+            .textContent
+            .replace(word, replaceWord.toUpperCase());
+        } else {
+          textNodes[i].textContent = textNodes[i]
+            .textContent
+            .replace(word, replaceWord);
+        };
+      };
     }
   }
-    // callback for removing the overlay
-    removeOverlay();
+  // callback for removing the overlay
+  removeOverlay();
 }
 
 // clicking on the chrome extension icon
-chrome.extension.onMessage.addListener( function(request, sender, sendResponse) {
+chrome
+  .extension
+  .onMessage
+  .addListener(function (request, sender, sendResponse) {
 
     // start translation
     if (request.method == "start") {
-        // ask the background script for the selected tab language
-        chrome.extension.sendMessage( { method: "getLanguage" }, function (response) {
-            if (response) {
-                language = response.data;
-                if(language == "pt-PT" || language == "pt") {
-                    translate (injectOverlayElement, reverseOverlay);
-                }
+      // ask the background script for the selected tab language
+      chrome
+        .extension
+        .sendMessage({
+          method: "getLanguage"
+        }, function (response) {
+          if (response) {
+            language = response.data;
+            if (language == "pt-PT" || language == "pt") {
+              translate(injectOverlayElement, reverseOverlay);
             }
+          }
         });
     };
-});
+  });
+  
